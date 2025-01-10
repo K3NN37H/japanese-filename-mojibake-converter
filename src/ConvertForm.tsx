8abc025ad98cd1a2ddfeb5cbd4bb623e49ffbafd
Ken <kjtchan@gmail.com>
2025-01-10 15:48:26 -0500

    Switch to using built-in TextDecoder
    - Fix input being permanently locked after one error

--- src/ConvertForm.tsx
+++ src/ConvertForm.tsx
@@ -15,10 +15,13 @@ export default function ConvertForm() {
       inputArea?.current?.setCustomValidity('')
       const normalized = inputText.normalize('NFC')
       const encoded = cptable.utils.encode(inputEncoding, normalized)
-      setConvertedText(cptable.utils.decode(932, encoded))
+      const encodedArray = new Uint8Array(encoded as Array<number>)
+      const decoder = new TextDecoder('Windows-31J')
+      setConvertedText(decoder.decode(encodedArray))
     } catch (e) {
+      console.log(e)
       inputArea?.current?.setCustomValidity(
-        "The input text couldn't be converted. The text might not be Windows mojibake."
+        "The input text couldn't be converted. The text might not be Windows mojibake, or the text was malformed beyond interpretation."
       )
     }
 
@@ -30,16 +33,18 @@ export default function ConvertForm() {
       <form onSubmit={convertText} className={layouts.formGrid}>
         <div>
           <label htmlFor='input'>Input</label>
-          <textarea ref={inputArea} id='input' rows={15} cols={100} value={inputText} onChange={(e) => setInputText(e.target.value)} />
+          <textarea ref={inputArea} id='input' rows={15} cols={100} value={inputText} onChange={(e) => {
+            e.target.setCustomValidity('')
+            setInputText(e.target.value)}} />
           <input type='submit' />
+          <details style={{ display: 'none' }}>
+            <summary>Advanced settings</summary>
+            <select value={inputEncoding} onChange={(e) => setInputEncoding(parseInt(e.target.value, 10))}>
+              <option value={437}>IBM code page 437 (Windows US English default)</option>
+              <option value={65001}>UTF-8</option>
+            </select>
+          </details>
         </div>
-        <details style={{ display: 'none' }}>
-          <summary>Advanced settings</summary>
-          <select value={inputEncoding} onChange={(e) => setInputEncoding(parseInt(e.target.value, 10))}>
-            <option value={437}>IBM code page 437 (Windows US English default)</option>
-            <option value={65001}>UTF-8</option>
-          </select>
-        </details>
         <div>
           <label htmlFor='result'>Result</label>
           <output>
